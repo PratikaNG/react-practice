a9e1b259f265eb6321f1057ada06bdb3c413ee2b
PratikaNG <67963086+PratikaNG@users.noreply.github.com>
2023-09-17 19:38:44 +0530

    Delete hello world directory

--- hello world/src/index.js
+++
@@ -1,6 +0,0 @@
-// import { render } from "@testing-library/react";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
